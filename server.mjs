7c9c39e55f08336a34d054ab87c47692e3b81ef7
malakalm <86434110+malakalm@users.noreply.github.com>
2025-05-19 17:18:53 +0530

    cs

--- server.mjs
+++ server.mjs
@@ -2,12 +2,14 @@ import express from 'express';
 import api from './src/route/api.mjs';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger.mjs'; // Add .js extension when using ES Modules
+import cors from 'cors'; // ✅ Add this line
 
 
 
 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
